496d2c50be19f8545e5326e1c1cb5d9bd32db5be
EduardoCarruci <edugocarruci@gmail.com>
2025-12-17 02:18:49 -0400

    Agregando Cubit a la Pagina de Game

--- lib/features/game/presentation/pages/game_page.dart
+++ lib/features/game/presentation/pages/game_page.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:numeroadivina/core/enums/difficulty.dart';
+import 'package:numeroadivina/features/game/presentation/cubit/game_cubit.dart';
 import '../widgets/widgets.dart';
 
-class GamePage extends StatefulWidget {
+class GamePage extends StatelessWidget {
   static const id = '/game';
-  const GamePage({super.key});
+  GamePage({super.key});
 
   static void pushNavigate(BuildContext context) => context.goNamed(id);
 
+  final GameCubit cubit = GetIt.I<GameCubit>();
+
   @override
-  State<GamePage> createState() => _GamePageState();
+  Widget build(BuildContext context) {
+    return BlocProvider.value(
+      value: cubit,
+      child: _GameView(cubit: cubit),
+    );
+  }
 }
 
-class _GamePageState extends State<GamePage> {
-  final TextEditingController _controller = TextEditingController();
-  double _difficulty = 0.0;
+class _GameView extends StatelessWidget {
+  _GameView({Key? key, required this.cubit}) : super(key: key);
+
+  final GameCubit cubit;
+  final _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _difficulties = [
+    Difficulty.facil,
+    Difficulty.medio,
+    Difficulty.avanzado,
+    Difficulty.extremo,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +56,11 @@ class _GamePageState extends State<GamePage> {
             const SizedBox(height: 16),
             const ThreeColumns(),
             const SizedBox(height: 12),
-            DifficultySlider(
+          /*   DifficultySlider(
               value: _difficulty,
               onChanged: (v) => setState(() => _difficulty = v),
               label: _difficulty < 0.5 ? 'easy' : 'hard',
-            ),
+            ), */
             const SizedBox(height: 12),
             PrimaryButton(
               text: 'Probar',
@@ -57,4 +77,3 @@ class _GamePageState extends State<GamePage> {
     );
   }
 }
-
